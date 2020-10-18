8c2b0a2bc217c23a60f791e04ec88d0443ca3b30
newtonnewton <56573308+newtonnewton@users.noreply.github.com>
2020-10-17 23:10:01 -0500

    v1

---
+++ src/components/elements/MovieThumb.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from '@reach/router';
+
+import { StyledMovieThumb } from '../styles/StyledMovieThumb';
+
+
+const MovieThumb = ({ image, movieId, clickable, }) => (
+    <StyledMovieThumb>
+      {
+          clickable? (
+              <Link to={`/${movieId}`}>
+                <img className ="clickable" src ={image} alt ="movieThumb" />
+              </Link>
+          ) : (
+              <img src ={image} alt="moviethumb" />
+          )
+      }
+    </StyledMovieThumb>
+)
+
+MovieThumb.propTypes = {
+    image : PropTypes.string,
+    movieId : PropTypes.number,
+    clickable : PropTypes.bool
+}
+
+export default MovieThumb;
